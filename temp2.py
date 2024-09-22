644995aaecf74f4ad00b871db5f73d04bc3b3e72
shahzeb-py <133527743+shahzeb-py@users.noreply.github.com>
2024-09-23 02:54:57 +0500

    Update temp2.py

--- temp2.py
+++ temp2.py
@@ -1,10 +1,7 @@
-	
 from selenium import webdriver
-options = webdriver.ChromeOptions()
-options.add_argument("--no-sandbox")
-options.add_argument("--disable-dev-shm-usage")
-options.add_argument("--headless=new")
-driver = webdriver.Chrome(options=options)
+options = webdriver.FirefoxOptions()
+options.add_argument("--headless")
+driver = webdriver.Firefox(options=options)
 driver.get("https://www.google.com")
-print("Please check screenshot image")
+driver.save_screenshot("/sdcard/download/screenshot.png")
 driver.quit()
