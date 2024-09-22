9752142902fa0573d8a8b042f41e70acea913ad2
shahzeb-py <133527743+shahzeb-py@users.noreply.github.com>
2024-09-23 01:24:11 +0500

    Update messageall.py

--- messageall.py
+++ messageall.py
@@ -18,96 +18,99 @@ total_chromes = int(input("Total Chromes: "))
 
 # Define the message function
 def message(account):
-    start_time = time.time()
-    post = "https://www.facebook.com/permalink.php?story_fbid=pfbid02DqogS977PeGU3ALXAawPRZxZXzpzNfgs7sSnp4brGYmCPnAMog3TFLtkqs9F2urJl&id=61565582983950"
-    uids = []
-    pattern = r"fbid=(\d+)"
-
-    # Chrome options setup
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")
-    chrome_options.add_argument('--disable-gpu')
-    chrome_options.add_argument('--disable-blink-features=AutomationControlled')
-    chrome_options.add_experimental_option('useAutomationExtension', False)
-    chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
-    chrome_options.add_argument("disable-infobars")
-    chrome_options.add_argument("--disable-notifications")
-    chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
-    chrome_options.add_argument("--window-size=1920x1080")  # Set a fixed window size
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_experimental_option("prefs", {
-    "profile.default_content_setting_values.notifications": 2})
-    chrome_options.add_argument('--disable-extensions')  # Disable extensions
-    chrome_options.add_argument('--start-maximized')  # Start maximized
-    chrome_options.add_argument('--disable-popup-blocking')  # Disable popup blocking
-    chrome_options.add_experimental_option('excludeSwitches', ['enable-automation', 'enable-logging'])
-    chrome_options.add_argument("--disable-blink-features=AutomationControlled")
-    # Start ChromeDriver
-    driver = webdriver.Chrome(options=chrome_options)
-    
     try:
-        driver.implicitly_wait(3)
-        driver.get("https://www.facebook.com/login/")
-        driver.find_element(By.XPATH, '//input[@id="email"]').send_keys(accounts[account][0])
-        driver.find_element(By.XPATH, '//input[@type="password"]').send_keys(accounts[account][1])
-        driver.find_element(By.XPATH, '//button[@id="loginbutton"]').click()
+        start_time = time.time()
+        post = "https://www.facebook.com/permalink.php?story_fbid=pfbid02DqogS977PeGU3ALXAawPRZxZXzpzNfgs7sSnp4brGYmCPnAMog3TFLtkqs9F2urJl&id=61565582983950"
+        uids = []
+        pattern = r"fbid=(\d+)"
+
+        # Chrome options setup
+        chrome_options = Options()
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument('--disable-gpu')
+        chrome_options.add_argument('--disable-blink-features=AutomationControlled')
+        chrome_options.add_experimental_option('useAutomationExtension', False)
+        chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        chrome_options.add_argument("disable-infobars")
+        chrome_options.add_argument("--disable-notifications")
+        chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
+        chrome_options.add_argument("--window-size=1920x1080")  # Set a fixed window size
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_experimental_option("prefs", {
+        "profile.default_content_setting_values.notifications": 2})
+        chrome_options.add_argument('--disable-extensions')  # Disable extensions
+        chrome_options.add_argument('--start-maximized')  # Start maximized
+        chrome_options.add_argument('--disable-popup-blocking')  # Disable popup blocking
+        chrome_options.add_experimental_option('excludeSwitches', ['enable-automation', 'enable-logging'])
+        chrome_options.add_argument("--disable-blink-features=AutomationControlled")
+        # Start ChromeDriver
+        driver = webdriver.Chrome(options=chrome_options)
         
-        # Check login status
-        current_url = driver.current_url
-        if "checkpoint" in current_url or "device-based/regular/login/?login_attempt" in current_url:
-            try:
-                driver.find_element(By.XPATH,'//div[@aria-label="Dismiss"]').click()
-            except:
-                print(f"{account} - login issue detected.")
+        try:
+            driver.implicitly_wait(3)
+            driver.get("https://www.facebook.com/login/")
+            driver.find_element(By.XPATH, '//input[@id="email"]').send_keys(accounts[account][0])
+            driver.find_element(By.XPATH, '//input[@type="password"]').send_keys(accounts[account][1])
+            driver.find_element(By.XPATH, '//button[@id="loginbutton"]').click()
+            
+            # Check login status
+            current_url = driver.current_url
+            if "checkpoint" in current_url or "device-based/regular/login/?login_attempt" in current_url:
+                try:
+                    driver.find_element(By.XPATH,'//div[@aria-label="Dismiss"]').click()
+                except:
+                    print(f"{account} - login issue detected.")
+                    driver.quit()
+                    return
+            if "two_step_verification" in current_url:
+                print(f"remove verification from {account}")
                 driver.quit()
                 return
-        if "two_step_verification" in current_url:
-            print(f"remove verification from {account}")
+            try:
+                driver.get("https://mbasic.facebook.com/buddylist.php")
+                active_elements = driver.find_elements(By.XPATH, '//a[@class="bq"]')
+
+                # Extract user IDs
+                for element in active_elements:
+                    href = element.get_attribute('href')
+                    if href:
+                        match = re.search(pattern, href)
+                        if match:
+                            uids.append(match.group(1))
+            except:
+                print("getting error in mbasic") 
+            # Send messages
+
+            num_uids = len(uids)
+            driver.implicitly_wait(20)
+            for uid in uids:
+                
+                try:
+                    driver.get(f"https://www.facebook.com/messages/t/{uid}")
+                    textbox = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//div[@aria-label="Message"]')))
+                    textbox.click()
+                    textbox.send_keys(post)
+                    textbox.send_keys(Keys.ENTER)
+                    WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, '//span[contains(text(),"Sent")]')))
+                    time.sleep(1)
+                except Exception as e:
+                    print(e)
+                    pass
+            time.sleep(2)
+            end_time = time.time()  # End timing
+            elapsed_time = end_time - start_time
+            minutes, seconds = divmod(elapsed_time, 60)
+            print(f"{num_uids} Messages by {account} time: {int(minutes)} minutes and {seconds:.2f} seconds")
             driver.quit()
             return
-        try:
-            driver.get("https://mbasic.facebook.com/buddylist.php")
-            active_elements = driver.find_elements(By.XPATH, '//a[@class="bq"]')
-
-            # Extract user IDs
-            for element in active_elements:
-                href = element.get_attribute('href')
-                if href:
-                    match = re.search(pattern, href)
-                    if match:
-                        uids.append(match.group(1))
-        except:
-            print("getting error in mbasic") 
-        # Send messages
-
-        num_uids = len(uids)
-        driver.implicitly_wait(20)
-        for uid in uids:
-            
-            try:
-                driver.get(f"https://www.facebook.com/messages/t/{uid}")
-                textbox = WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.XPATH, '//div[@aria-label="Message"]')))
-                textbox.click()
-                textbox.send_keys(post)
-                textbox.send_keys(Keys.ENTER)
-                WebDriverWait(driver, 30).until(EC.presence_of_element_located((By.XPATH, '//span[contains(text(),"Sent")]')))
-                time.sleep(1)
-            except Exception as e:
-                print(e)
-                pass
-        time.sleep(2)
-        end_time = time.time()  # End timing
-        elapsed_time = end_time - start_time
-        minutes, seconds = divmod(elapsed_time, 60)
-        print(f"{num_uids} Messages by {account} time: {int(minutes)} minutes and {seconds:.2f} seconds")
-        driver.quit()
+        except :
+            driver.quit()
+            print(f"having issue in {account}")
+            return
+    except:
+        print("whole program issue")
         return
-    except :
-        driver.quit()
-        print(f"having issue in {account}")
-        return
-
 
 start_index = start_index
 max_workers = total_chromes
