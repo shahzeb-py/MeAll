c701d18e52d8eeca3016ac20b4a38cf403bc666c
shahzeb-py <133527743+shahzeb-py@users.noreply.github.com>
2024-09-30 20:52:21 +0500

    Update temp2.py

--- temp2.py
+++ temp2.py
@@ -8,7 +8,6 @@ from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import Options
 
-from selenium.webdriver.chrome.options import Options
 firstnam  = [
     'Aarohi', 'Abhilasha', 'Aishwarya', 'Akanksha', 'Alka', 'Amita', 'Ankita', 
     'Aparna', 'Aradhya', 'Avantika', 'Bhoomika', 'Chandni', 'Charvi', 'Chitra', 
