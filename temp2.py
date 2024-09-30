70ababe1d61f9f409ed3226a41c3271c74428044
shahzeb-py <133527743+shahzeb-py@users.noreply.github.com>
2024-09-30 21:07:05 +0500

    Update temp2.py

--- temp2.py
+++ temp2.py
@@ -1,5 +1,7 @@
-
+from selenium.webdriver.common.by import By
 import random
+import time
+from selenium.webdriver.common.keys import Keys
 from selenium import webdriver
 from selenium.webdriver.firefox.service import Service
 from selenium.webdriver.firefox.options import Options
@@ -72,10 +74,120 @@ firefox_options.set_preference("general.useragent.override", custom_user_agent)
 
 # Start the WebDriver
 def Create():
+    random_number = str(random.randint(1000000, 9999999))
+    random_numbers = str(random.randint(100, 99999))
+    forth= random.randint(1,8)
+    third= random.randint(0,4)
+    fn = random.choice(firstnam)
+    ln = random.choice(lastnam)
+    dt = random.choice(date)
+    mt = random.choice(month)
+    yr = random.choice(year)
     driver = webdriver.Firefox(service=service, options=firefox_options)
-    driver.get("https://www.whatismybrowser.com/")  
-    user_agent = driver.execute_script("return navigator.userAgent;")
-    print(f"User Agent {user_agent}")
+    try:
+        driver.implicitly_wait(6)
+        GetStarted= driver.find_element(By.XPATH,'//div[@aria-label="Get Started" and @role="button"]')
+        GetStarted.click()
+    except:
+        driver.quit()
+        return
+    try:
+        firstname = driver.find_element(By.XPATH,'//input[@aria-label="First name"]')
+        firstname.send_keys(fn)
+        time.sleep(1)
+        lastname = driver.find_element(By.XPATH,'//input[@aria-label="Surname"]')
+        lastname.send_keys(ln)
+        time.sleep(1)
+        next = driver.find_element(By.XPATH,'//div[@aria-label="Next" and @role="button"]')
+        next.click()
+    
+        
+        date_input = driver.find_element(By.CSS_SELECTOR, 'input[type="date"]')
+        date_input.clear()
+        time.sleep(1)
+        date_input.send_keys(f'{mt}{dt}{yr}')
+        date_input.send_keys(Keys.RETURN)
+        next = driver.find_element(By.XPATH,'//div[@aria-label="Next" and @role="button"]')
+        next.click()
+        female = driver.find_element(By.XPATH,'//div[@aria-label="Female"]')
+        #male = driver.find_element(By.XPATH,'//div[@aria-label="Male"]')
+        #gender_elements = [female, male]
+        gender = female#random.choice(gender_elements)
+        gender.click()
+        next = driver.find_element(By.XPATH,'//div[@aria-label="Next" and @role="button"]')
+        next.click()
+        time.sleep(6)
+        Number= driver.find_element(By.XPATH,'//input[@aria-label="Mobile number"]')
+        Number.click()
+        Number.send_keys(f"03{third}{forth} {random_number}")
+        next = driver.find_element(By.XPATH,'//div[@aria-label="Next" and @role="button"]')
+        next.click()
+    
+        putpassword = driver.find_element(By.XPATH,'//input[@aria-label="Password"]')
+        password=f"{fn}{yr}gf2S"
+        putpassword.send_keys(password)
+        next = driver.find_element(By.XPATH,'//div[@aria-label="Next" and @role="button"]')
+        next.click()
+    except:
+            
+        driver.quit()
+        return 
+    
+    savelogin=driver.find_element(By.XPATH,'//div[@aria-label="Save"]')
+    savelogin.click()
+    agreeterms=driver.find_element(By.XPATH,'//div[@aria-label="I agree"]')
+    agreeterms.click()
+    timee =random.randint(6,8)
+    time.sleep(timee)
+    driver.implicitly_wait(30)
+    
+    try:
+        driver.find_element(By.XPATH,'//div[@aria-label="Continue"]').click()
+    except:
+            
+            pass
+    driver.implicitly_wait(5)
+    driver.get("https://m.facebook.com/confirmemail.php?soft=hjk")
+    try:
+        didNotRecieved = driver.find_element(By.LINK_TEXT,"I Didn't Receive the Code")
+        didNotRecieved.click()
+        sendcodeagain = driver.find_element(By.LINK_TEXT,"Send Code Again")
+        sendcodeagain.click()
+        didNotRecieved = driver.find_element(By.LINK_TEXT,"I Didn't Receive the Code")
+        didNotRecieved.click()
+    except:
+        try:
+            didNotRecieved = driver.find_element(By.LINK_TEXT,"I didn't get the code")
+            didNotRecieved.click()
+            sendcodeagain = driver.find_element(By.LINK_TEXT,"Send Code Again")
+            sendcodeagain.click()
+            didNotRecieved = driver.find_element(By.LINK_TEXT,"I didn't get the code")
+            didNotRecieved.click()
+        except:
+                
+            driver.quit()
+            return  
+    confirmbyemail= driver.find_element(By.XPATH,'//a[@class="_6if8 _8x0i _8xqj _8x0k" and @href="/changeemail/" and @target="_self" and @sigil="no_mpc"]')
+    confirmbyemail.click()
+    email= driver.find_element(By.XPATH,'//input[@name="new"]')
+    email.click()
 
-
+    copiedText = input("enter email")
+    try:
+        email.send_keys(copiedText)
+        add= driver.find_element(By.XPATH,'//button[@type="submit" and @value="Add"]')
+        add.click()
+    except:
+        driver.quit()
+        return 
+    code=input("enter code")
+    putcode = driver.find_element(By.XPATH,'//input[@type="number"]')
+    putcode.click()
+    putcode.send_keys(code)
+    confirm= driver.find_element(By.XPATH,"//a[contains(text(), 'Confirm')]")
+    confirm.click()
+    print(f"{copiedText}|{password}")
+    time.sleep(10)
+    driver.quit()
+    return
 Create()
