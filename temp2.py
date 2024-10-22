73e5b0e733865f6c496dd6b724ed2d2c8adc1b42
shahzeb-py <133527743+shahzeb-py@users.noreply.github.com>
2024-10-22 16:41:01 +0500

    Update temp2.py

--- temp2.py
+++ temp2.py
@@ -75,133 +75,141 @@ firefox_options.set_preference("general.useragent.override", custom_user_agent)
 
 # Start the WebDriver
 def Create():
-    random_number = str(random.randint(1000000, 9999999))
-    random_numbers = str(random.randint(100, 99999))
-    forth= random.randint(1,8)
-    third= random.randint(0,4)
-    fn = random.choice(firstnam)
-    ln = random.choice(lastnam)
-    dt = random.choice(date)
-    mt = random.choice(month)
-    yr = random.choice(year)
-    driver = webdriver.Firefox(service=service, options=firefox_options)
-    #driver = webdriver.Firefox(options=firefox_options)
-    driver.get("https://m.facebook.com/reg/#")
     try:
-        driver.implicitly_wait(6)
-        GetStarted= driver.find_element(By.XPATH,'//div[@aria-label="Get Started" and @role="button"]')
-        GetStarted.click()
-        print("get started done")
-    except:
-        print("error in get started")
-        driver.quit()
-        return
-    try:
-        firstname = driver.find_element(By.XPATH,'//input[@aria-label="First name"]')
-        firstname.send_keys(fn)
-        time.sleep(1)
-        lastname = driver.find_element(By.XPATH,'//input[@aria-label="Surname"]')
-        lastname.send_keys(ln)
-        time.sleep(1)
-        next = driver.find_element(By.XPATH,'//div[@aria-label="Next" and @role="button"]')
-        next.click()
-        print(" firtstname done")
-        
-        input_field = driver.find_element(By.CSS_SELECTOR, 'input[type="date"]')
-        actions = ActionChains(driver)
-        actions.move_to_element(input_field).click().perform()
-        for char in f"0{mt}{dt}{yr}":
-            actions.send_keys(char).pause(0.1)
-        actions.perform()
-        time.sleep(1)
-        next = driver.find_element(By.XPATH,'//div[@aria-label="Next" and @role="button"]')
-        next.click()
-        
-        print(" dob done")
-        female = driver.find_element(By.XPATH,'//div[@aria-label="Female"]')
-        #male = driver.find_element(By.XPATH,'//div[@aria-label="Male"]')
-        #gender_elements = [female, male]
-        gender = female#random.choice(gender_elements)
-        gender.click()
-        print(" gender done")
-        next = driver.find_element(By.XPATH,'//div[@aria-label="Next" and @role="button"]')
-        next.click()
-        time.sleep(6)
-        Number= driver.find_element(By.XPATH,'//input[@aria-label="Mobile number"]')
-        Number.click()
-        Number.send_keys(f"03{1}{5}{random_number}")
-        next = driver.find_element(By.XPATH,'//div[@aria-label="Next" and @role="button"]')
-        next.click()
-        print(" number done")
-        putpassword = driver.find_element(By.XPATH,'//input[@aria-label="Password"]')
-        password=f"{fn}{yr}gf2S"
-        putpassword.send_keys(password)
-        next = driver.find_element(By.XPATH,'//div[@aria-label="Next" and @role="button"]')
-        next.click()
-    except:
-            
-        driver.quit()
-        return 
-    driver.implicitly_wait(2)
-    try:
-        savelogin=driver.find_element(By.XPATH,'//div[@aria-label="Save"]')
-        savelogin.click()
-    except:
-        pass
-    agreeterms=driver.find_element(By.XPATH,'//div[@aria-label="I agree"]')
-    agreeterms.click()
-    timee =random.randint(6,8)
-    time.sleep(20)
-    driver.refresh()
-    urll=driver.current_url
-    if "checkpoint" in urll:
-        print("checkpoint")
-        driver.quit()
-        return
-    try:
-
-        driver.find_element(By.XPATH,'//div[@aria-label="Continue"]').click()
-        print("continue")
-    except: 
-        pass
-    try:
-        driver.implicitly_wait(5)
-        driver.find_element(By.XPATH,'//button[@value="Send code"]').click()
-    except:
-        pass
-    try:
-        didNotRecieved = driver.find_element(By.LINK_TEXT,"I Didn't Receive the Code")
-        didNotRecieved.click()
-    except:
+        random_number = str(random.randint(1000000, 9999999))
+        random_numbers = str(random.randint(100, 99999))
+        forth= random.randint(1,8)
+        third= random.randint(0,4)
+        fn = random.choice(firstnam)
+        ln = random.choice(lastnam)
+        dt = random.choice(date)
+        mt = random.choice(month)
+        yr = random.choice(year)
+        driver = webdriver.Firefox(service=service, options=firefox_options)
+        #driver = webdriver.Firefox(options=firefox_options)
+        driver.get("https://m.facebook.com/reg/#")
         try:
-            didNotRecieved = driver.find_element(By.LINK_TEXT,"I didn't get the code")
+            driver.implicitly_wait(6)
+            GetStarted= driver.find_element(By.XPATH,'//div[@aria-label="Get Started" and @role="button"]')
+            GetStarted.click()
+            print("get started done")
+        except:
+            print("error in get started")
+            driver.quit()
+            return
+        try:
+            firstname = driver.find_element(By.XPATH,'//input[@aria-label="First name"]')
+            firstname.send_keys(fn)
+            time.sleep(1)
+            lastname = driver.find_element(By.XPATH,'//input[@aria-label="Surname"]')
+            lastname.send_keys(ln)
+            time.sleep(1)
+            next = driver.find_element(By.XPATH,'//div[@aria-label="Next" and @role="button"]')
+            next.click()
+            print(" firtstname done")
+            
+            input_field = driver.find_element(By.CSS_SELECTOR, 'input[type="date"]')
+            actions = ActionChains(driver)
+            actions.move_to_element(input_field).click().perform()
+            for char in f"0{mt}{dt}{yr}":
+                actions.send_keys(char).pause(0.1)
+            actions.perform()
+            time.sleep(1)
+            next = driver.find_element(By.XPATH,'//div[@aria-label="Next" and @role="button"]')
+            next.click()
+            
+            print(" dob done")
+            female = driver.find_element(By.XPATH,'//div[@aria-label="Female"]')
+            #male = driver.find_element(By.XPATH,'//div[@aria-label="Male"]')
+            #gender_elements = [female, male]
+            gender = female#random.choice(gender_elements)
+            gender.click()
+            print(" gender done")
+            next = driver.find_element(By.XPATH,'//div[@aria-label="Next" and @role="button"]')
+            next.click()
+            time.sleep(6)
+            Number= driver.find_element(By.XPATH,'//input[@aria-label="Mobile number"]')
+            Number.click()
+            Number.send_keys(f"03{1}{5}{random_number}")
+            next = driver.find_element(By.XPATH,'//div[@aria-label="Next" and @role="button"]')
+            next.click()
+            print(" number done")
+            putpassword = driver.find_element(By.XPATH,'//input[@aria-label="Password"]')
+            password=f"{fn}{yr}gf2S"
+            putpassword.send_keys(password)
+            next = driver.find_element(By.XPATH,'//div[@aria-label="Next" and @role="button"]')
+            next.click()
+        except:
+                
+            driver.quit()
+            return 
+        driver.implicitly_wait(2)
+        try:
+            savelogin=driver.find_element(By.XPATH,'//div[@aria-label="Save"]')
+            savelogin.click()
+        except:
+            pass
+        try:
+            agreeterms=driver.find_element(By.XPATH,'//div[@aria-label="I agree"]')
+            agreeterms.click()
+        except:
+            pass
+        timee =random.randint(6,8)
+        time.sleep(20)
+        driver.refresh()
+        urll=driver.current_url
+        if "checkpoint" in urll:
+            print("checkpoint")
+            driver.quit()
+            return
+        try:
+
+            driver.find_element(By.XPATH,'//div[@aria-label="Continue"]').click()
+            print("continue")
+        except: 
+            pass
+        try:
+            driver.implicitly_wait(5)
+            driver.find_element(By.XPATH,'//button[@value="Send code"]').click()
+        except:
+            pass
+        try:
+            didNotRecieved = driver.find_element(By.LINK_TEXT,"I Didn't Receive the Code")
             didNotRecieved.click()
         except:
-            driver.quit()
-            return  
-    confirmbyemail= driver.find_element(By.XPATH,'//a[@class="_6if8 _8x0i _8xqj _8x0k" and @href="/changeemail/" and @target="_self" and @sigil="no_mpc"]')
-    confirmbyemail.click()
-    email= driver.find_element(By.XPATH,'//input[@name="new"]')
-    email.click()
+            try:
+                didNotRecieved = driver.find_element(By.LINK_TEXT,"I didn't get the code")
+                didNotRecieved.click()
+            except:
+                driver.quit()
+                return  
+        confirmbyemail= driver.find_element(By.XPATH,'//a[@class="_6if8 _8x0i _8xqj _8x0k" and @href="/changeemail/" and @target="_self" and @sigil="no_mpc"]')
+        confirmbyemail.click()
+        email= driver.find_element(By.XPATH,'//input[@name="new"]')
+        email.click()
 
-    copiedText = input("enter email")
-    try:
-        email.send_keys(copiedText)
-        add= driver.find_element(By.XPATH,'//button[@type="submit" and @value="Add"]')
-        add.click()
-    except:
+        copiedText = input("enter email")
+        try:
+            email.send_keys(copiedText)
+            add= driver.find_element(By.XPATH,'//button[@type="submit" and @value="Add"]')
+            add.click()
+        except:
+            driver.quit()
+            return 
+        code=input("enter code")
+        putcode = driver.find_element(By.XPATH,'//input[@type="number"]')
+        putcode.click()
+        putcode.send_keys(code)
+        confirm= driver.find_element(By.XPATH,"//a[contains(text(), 'Confirm')]")
+        confirm.click()
+        print(f"{copiedText}|{password}")
+        time.sleep(10)
         driver.quit()
-        return 
-    code=input("enter code")
-    putcode = driver.find_element(By.XPATH,'//input[@type="number"]')
-    putcode.click()
-    putcode.send_keys(code)
-    confirm= driver.find_element(By.XPATH,"//a[contains(text(), 'Confirm')]")
-    confirm.click()
-    print(f"{copiedText}|{password}")
-    time.sleep(10)
-    driver.quit()
-    return
+        return
+    except:
+        driver.save_screenshot(f'/sdcard/screenshot{random_number}.png')
+        driver.quit()
+        return
 Create()
 """
 //div[@aria-label="Not now"]
